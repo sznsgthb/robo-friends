e0fe8106aa6ece08d383a66a12e2b6213d2efe18
sznsgthb <suzannehoftijzer@gmail.com>
2026-03-15 21:34:58 +0100

    code fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/robo-friends/'  // <- voeg hier je repo naam toe
+    base: '/robo-friends/',
 })
